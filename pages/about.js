286b5dd62ecc9a9734072b8bcb0add2903de52da
Isuru Ranaweera <isuruar@gmail.com>
2021-04-06 13:11:24 +0530

    initial

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+const About = () => {
+    return ( 
+    <>
+        <Head> 
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas"/>
+        </Head>
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur voluptatem fugit eveniet quidem, laudantium amet ipsum quibusdam quo facere recusandae accusamus, repudiandae expedita nihil, consequatur doloremque inventore velit odio unde?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur voluptatem fugit eveniet quidem, laudantium amet ipsum quibusdam quo facere recusandae accusamus, repudiandae expedita nihil, consequatur doloremque inventore velit odio unde?</p>
+        </div>
+    </>
+
+    );
+}
+ 
+export default About;
